5804d48058d7ad9221e8eefeb193e021025937f9
meron <merronmuche@gmail.com>
2022-11-04 09:40:05 -0300

    add meetup detail and registration success htmls

--- meetups/urls.py
+++ meetups/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 from meetups import views
 
 urlpatterns = [
- path('',views.index)
+ path('',views.index),
+ path('<int:meetup_id>/', views.meetup_details, name='meetup-detail'),
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -7,3 +7,12 @@ def index(request):
         'meetups': meetups
     }
     return render(request,'meetups/includes/index.html',data)
+def meetup_details(request,meetup_id):
+
+    selected_meetup = Meetup.objects.get(id=meetup_id)
+    
+    data = {
+            'meetup_found': True,
+            'meetup': selected_meetup,
+                }
+    return render(request, 'meetups/includes/meetup_detail.html', data)
