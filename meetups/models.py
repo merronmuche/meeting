47342882dec3e2f28aa46ac3251f8a93bbd5dbae
meron <merronmuche@gmail.com>
2022-11-04 05:50:59 -0300

    created meetup model

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    
+    title=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    slug=models.CharField(max_length=100)
