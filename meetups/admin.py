804661a5346d6a7863c6130a6226d82ff4c40d73
meron <merronmuche@gmail.com>
2022-11-04 16:20:36 -0300

    Add all models on admin

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from meetups.models import Location, Meetup, Participant
+
+class LocationAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'address']
+
+admin.site.register(Location, LocationAdmin)
+
+class MeetupAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'organizer_email','date']
+
+admin.site.register(Meetup, MeetupAdmin)
+
+class ParticipantAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'email']
+
+admin.site.register(Participant, ParticipantAdmin)
