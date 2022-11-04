518c135fd3b4008754446ae6f57332a7f65dca71
meron <merronmuche@gmail.com>
2022-11-04 06:52:48 -0300

    add data

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from meetups.models import Meetup
 
 def index(request):
-
-    return render(request,'meetups/index.html')
+    meetups=Meetup.objects.all()
+    data = {
+        'meetups': meetups
+    }
+    return render(request,'meetups/index.html',data)
